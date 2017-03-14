43a1affd3ae98a271eafa895baf6ccd2e244b76b
Gwyn Jones <m@gwnjns.com>
2017-03-14 18:27:52 +0000

    Introduce Modernizr
    Custom build of Modernizr added with single detect:
    for IndexedDb. Includes tests for Modernizr and the
    necessary IndexedDb detection.

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,8 @@
 QUnit.test("jQuery exists", function (assert) {
     assert.equal(typeof window.$, 'function', "jQuery exists");
 });
+
+QUnit.test("Modernizr exists", function (assert) {
+    assert.equal(typeof Modernizr, 'object', "Modernizr exists");
+    assert.equal(typeof Modernizr.indexeddb, 'object', "Modernizr detect for IndexedDb is present");
+});
