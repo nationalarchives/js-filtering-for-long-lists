4deb81a4e3d850c641d95c4e4f20e98ccee3c07e
Gwyn Jones <m@gwnjns.com>
2017-03-15 15:39:13 +0000

    fixup! Add test to indicate JavaScript and IndexedDb are available

--- tests/tests.js
+++ tests/tests.js
@@ -12,3 +12,8 @@ QUnit.test("Modernizr exists", function (assert) {
         assert.ok($('html').hasClass(index), "Modernizr has added " + index + " class to html")
     })
 });
+<<<<<<< HEAD
+=======
+
+QUnit.test("Every ")
+>>>>>>> 1a4ccb2... Introduce JS and CSS files for application code
