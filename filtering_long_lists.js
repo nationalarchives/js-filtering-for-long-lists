12ccfd1e97f9ee6de0242a4fe4c1ea7b4b321bf4
Gwyn Jones <m@gwnjns.com>
2017-03-15 22:13:12 +0000

    Add client-side database and populate it
    This is achieved using a combination of jQuery and DexieJS (as a proxy
    to IndexedDB). Functions introduced are:
    
    * extract_data_from_html() which returns an array of objects that
      describe every jQuery wrapped link passed to it.
    * connect_to_or_set_up_database() which uses Dexie to create and populate
      the database if it does not exist, or simply connect to it if it does.
      Note: this script will only populate the database on creation, not every
      time the page is loaded.

--- filtering_long_lists.js
+++ filtering_long_lists.js
@@ -1,11 +1,56 @@
 // Using Revealing Module Pattern
 
-var TNALongLists = (function($) {
+var TNALongLists = (function ($, Dexie) {
 
     var $links = $('.az a');
 
-    return {
-        $links: $links
+    var extract_data_from_html = function ($els) {
+
+        var data_from_html = [];
+
+        $els.each(function (i) {
+            var $this = $(this),
+                text = $this.text(),
+                url = $this.attr('href');
+
+            data_from_html.push({id: i, text: text, url: url});
+        });
+
+        return data_from_html;
     };
 
-})(jQuery);
+
+    var connect_to_or_set_up_database = function (data) {
+
+        var db = new Dexie("links_database");
+
+        db.version(1).stores({links_data: 'id,text,url'});
+
+        db.on("populate", function () {
+            db.links_data.bulkAdd(data).then(function (lastKey) {
+                console && console.log(lastKey + ' item(s) added to database');
+            }).catch(Dexie.BulkError, function (e) {
+                console && console.error("Some items were not added successfully");
+            });
+        });
+
+        db.open().then(function (db) {
+            console && console.log("Database opened successfully");
+        }).catch(function (err) {
+            console && console.log("Error : " + err);
+        });
+
+        return db;
+    };
+
+    var data = extract_data_from_html($links);
+
+    var db = connect_to_or_set_up_database(data);
+
+    return {
+        $links: $links,
+        db: db
+    };
+
+})(jQuery, Dexie);
+
