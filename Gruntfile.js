61b91cad13cfbb48e826f184a691e5ab15739f84
Gwyn Jones <m@gwnjns.com>
2017-03-17 17:14:02 +0000

    Introduce Babel to compile ES2015 down
    Also sets up the necessary folder structure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,26 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['babel-preset-es2015']
+            },
+            dist: {
+                files: {
+                    'js/dist/filtering_long_lists.js': 'js/src/filtering_long_lists.js'
+                }
+            }
+        },
         qunit: {
             all: ['tests/test-runner.html']
         }
     });
 
+    grunt.loadNpmTasks('grunt-babel');
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // Default task(s).
-    grunt.registerTask('default', ['qunit']);
+    grunt.registerTask('default', ['babel', 'qunit']);
 
 };
---
+++ js/dist/filtering_long_lists.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// Using Revealing Module Pattern
+
+var TNALongLists = function ($, Dexie) {
+
+    var $links = $('.az a');
+
+    var extract_data_from_html = function extract_data_from_html($els) {
+
+        var data_from_html = [];
+
+        $els.each(function (i) {
+            var $this = $(this),
+                text = $this.text(),
+                url = $this.attr('href');
+
+            data_from_html.push({ id: i, text: text, url: url });
+        });
+
+        return data_from_html;
+    };
+
+    var connect_to_or_set_up_database = function connect_to_or_set_up_database(data) {
+
+        var db = new Dexie("links_database");
+
+        db.version(1).stores({ links_data: 'id,text,url' });
+
+        db.on("populate", function () {
+            db.links_data.bulkAdd(data).then(function (lastKey) {
+                console && console.log(lastKey + ' item(s) added to database');
+            }).catch(Dexie.BulkError, function (e) {
+                console && console.error("Some items were not added successfully");
+            });
+        });
+
+        db.open().then(function (db) {
+            console && console.log("Database opened successfully");
+        }).catch(function (err) {
+            console && console.log("Error : " + err);
+        });
+
+        return db;
+    };
+
+    var data = extract_data_from_html($links);
+
+    var db = connect_to_or_set_up_database(data);
+
+    return {
+        $links: $links,
+        db: db
+    };
+}(jQuery, Dexie);
+//# sourceMappingURL=filtering_long_lists.js.map
